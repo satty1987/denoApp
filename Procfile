7f9ee541ce00cb7d623661ad2f305add42208504
Satnam Singh <satnam.singh156@t-mobile.com>
2020-06-05 21:23:13 +0530

    update

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: deno run --allow-all --cached-only --unstable server.ts --port=${PORT}
--- server.ts
+++ server.ts
@@ -1,11 +1,21 @@
 import { Application } from 'https://deno.land/x/oak/mod.ts'
 import router from './routes.ts'
-import { NOT_FOUND ,ERROR_HANDLER} from './utils/middlewares.ts'
+import { NOT_FOUND ,ERROR_HANDLER} from './utils/middlewares.ts';
+import { organ } from "https://raw.githubusercontent.com/denjucks/organ/master/mod.ts";
+import * as flags from "https://deno.land/std/flags/mod.ts";
 
+const { args, exit } = Deno;
+const DEFAULT_PORT = 8000;
+const argPort = flags.parse(args).port;
+const port = argPort ? Number(argPort) : DEFAULT_PORT;
 
-const port = 8000
+if (isNaN(port)) {
+    console.log("port is not number");
+    exit(1);
+}
 
 const app = new Application()
+app.use(organ("short", true));
 
 
 app.use(ERROR_HANDLER);
@@ -14,6 +24,6 @@ app.use(router.routes());
 app.use(router.allowedMethods());
 app.use(NOT_FOUND);
 
-console.log(`Server running on port ${port}`)
+console.log(`Server running on port = ${port}`)
 
 await app.listen({ port })
